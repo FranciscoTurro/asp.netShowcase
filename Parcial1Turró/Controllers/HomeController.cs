6593811c6fe1afad347ed118bd6f111011e3282d
FranciscoTurro <franciscogtrro@outlook.com.ar>
2023-05-20 15:15:27 -0300

    consultas

--- Parcial1Turró/Controllers/HomeController.cs
+++ Parcial1Turró/Controllers/HomeController.cs
@@ -35,7 +35,47 @@ namespace Parcial1Turró.Controllers
 
         public IActionResult Consultas()
         {
-            return View();
+            var clientes = ObtenerClientes();
+            return View(clientes);
+        }
+
+        private List<Cliente> ObtenerClientes()
+        {
+            return new List<Cliente>
+            {
+                new Cliente
+                {
+                    email = "pepito@gmail.com",
+                    razonSocial = 11111111,
+                    direccion = "Calle 1400",
+                    localidad = "Rosario",
+                    pais = "Argentina"
+                },
+                new Cliente
+                {
+                    email = "jorgito@gmail.com",
+                    razonSocial = 22222222,
+                    direccion = "Calle 2112",
+                    localidad = "Brasilia",
+                    pais = "Brasil"
+                },
+                new Cliente
+                {
+                    email = "rodrigo@gmail.com",
+                    razonSocial = 33333333,
+                    direccion = "Calle 2478",
+                    localidad = "Toronto",
+                    pais = "Canada"
+                },
+                new Cliente
+                {
+                    email = "florentino@gmail.com",
+                    razonSocial = 44444444,
+                    direccion = "Calle 9876",
+                    localidad = "Madrid",
+                    pais = "España"
+                }
+            };
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Parcial1Turró/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace Parcial1Turró.Models
+{
+    public class Cliente
+    {
+        public string email { get; set; }
+        public int razonSocial { get; set; }
+        public string direccion { get; set; }
+        public string localidad { get; set; }
+        public string pais { get; set; }
+
+    }
+}
